c16f306ec1472710865c2c0bcc62fdf7cacfc427
TopDevSteven <cheerman00526@gmail.com>
2023-09-05 11:00:57 -0700

    updated the a tag link

--- src/components/BotMessageBox.jsx
+++ src/components/BotMessageBox.jsx
@@ -6,7 +6,16 @@ const BotMesssageBox = ({message}) => {
     return (
         <div className="bot-message">
             {
-                message === "LOADING" ? <p><SyncLoader color="#d2d2d2" size={12}/></p> : message.split("\n").map((item, idx) => <p key={idx}>{item}</p>)
+                message === "LOADING" 
+                ? <p><SyncLoader color="#d2d2d2" size={12}/></p> 
+                : message.split("\n").map((item, idx) => 
+                    <p key={idx}>
+                        { 
+                            item.replace(/(http:\/\/[^\s]+)/g, (match) => 
+                                <a href={match} target="_blank" rel="noopener noreferrer">{match}</a>
+                            )
+                        }
+                    </p>)
             }
         </div>
     )
