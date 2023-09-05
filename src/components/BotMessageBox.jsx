3b25379f94102beab65c6f9117621a4431a0ea07
TopDevSteven <cheerman00526@gmail.com>
2023-09-05 07:26:41 -0700

    refund

--- src/components/BotMessageBox.jsx
+++ src/components/BotMessageBox.jsx
@@ -2,36 +2,14 @@ import React from "react";
 import "./BotMessageBox.css";
 import { SyncLoader } from "react-spinners";
 
-const BotMessageBox = ({ message }) => {
-    const renderMessage = (text) => {
-        const urlRegex = /(http:\/\/|https:\/\/)\S+/g;
-        const parts = text.split(urlRegex);
-
-        return parts.map((part, idx) => {
-            if (urlRegex.test(part)) {
-                const url = part.trim();
-                return (
-                    <a key={idx} href={url} target="_blank" rel="noopener noreferrer">
-                        {url}
-                    </a>
-                );
-            } else {
-                return <span key={idx}>{part}</span>;
-            }
-        });
-    };
-
+const BotMesssageBox = ({message}) => {
     return (
         <div className="bot-message">
-            {message === "LOADING" ? (
-                <p>
-                    <SyncLoader color="#d2d2d2" size={12} />
-                </p>
-            ) : (
-                <div>{renderMessage(message)}</div>
-            )}
+            {
+                message === "LOADING" ? <p><SyncLoader color="#d2d2d2" size={12}/></p> : message.split("\n").map((item, idx) => <p key={idx}>{item}</p>)
+            }
         </div>
-    );
-};
+    )
+}
 
-export default BotMessageBox;
+export default BotMesssageBox;
