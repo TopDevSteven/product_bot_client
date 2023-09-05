0fb9180fae227889d9d40077e059a1d3778f090c
TopDevSteven <cheerman00526@gmail.com>
2023-09-05 11:10:50 -0700

    updated the tag

--- src/components/BotMessageBox.jsx
+++ src/components/BotMessageBox.jsx
@@ -9,13 +9,12 @@ const BotMesssageBox = ({message}) => {
                 message === "LOADING" 
                 ? <p><SyncLoader color="#d2d2d2" size={12}/></p> 
                 : message.split("\n").map((item, idx) => 
-                    <p key={idx}>
-                        { 
-                            item.replace(/(http:\/\/[^\s]+)/g, (match) => 
-                                <a href={match} target="_blank" rel="noopener noreferrer">{match}</a>
-                            )
-                        }
-                    </p>)
+                    <p key={idx} dangerouslySetInnerHTML={{ 
+                        __html: item.replace(/(http:\/\/[^\s]+)/g, (match) => 
+                            `<a href="${match}" target="_blank" rel="noopener noreferrer">${match}</a>`
+                        )
+                    }} />
+                )
             }
         </div>
     )
